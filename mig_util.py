055aabc1a79b544c5c97e35d7d32ab0e202d3e45
Scarecrow <30888091+ReaperZ0v@users.noreply.github.com>
2020-07-18 14:37:22 +0500

    Add files via upload

---
+++ mig_util.py
@@ -0,0 +1,44 @@
+import shutil
+import sys
+import os
+
+def remove_migs(project_folder, app_folder):
+    CACHE_TARGET = "__pycache__"
+    MIGRATIONS_TARGET = "migrations"
+    DB_TARGET = "db.sqlite3"
+
+    targets = [CACHE_TARGET, MIGRATIONS_TARGET]
+    if os.path.exists(project_folder):
+        sys.stdout.write(f"[+] Project folder {project_folder} detected \n".format())
+        os.chdir(project_folder)
+        for tgt in targets:
+            if os.path.exists(tgt):
+                shutil.rmtree(tgt, ignore_errors=True)
+
+            else:
+                print("[!] Path not located")
+                continue 
+
+    else:
+        sys.stdout.write(f"[!] Project folder {project_folder} not found".format())
+
+    os.chdir("..")
+    if os.path.exists(app_folder):
+        sys.stdout.write(f"[+] App folder {app_folder} detected \n".format())
+        os.chdir(app_folder)
+        for tgt in targets:
+            if os.path.exists(tgt):
+                shutil.rmtree(tgt, ignore_errors=True)
+
+            else:
+                print("[!] Path not located")
+                continue 
+
+    else:
+        sys.stdout.write(f"[!] App folder {app_folder} not found \n".format())
+
+
+project_folder = input("Django Project Folder > ")
+app_folder = input("Django App Folder > ")
+
+remove_migs(project_folder, app_folder)
